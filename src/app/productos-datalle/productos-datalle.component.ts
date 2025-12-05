b61c0038e90fe99682cc174e2931e8c3c2e3a3a7
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-12-05 02:22:52 +0100

    refact

--- src/app/productos-datalle/productos-datalle.component.ts
+++ src/app/productos-datalle/productos-datalle.component.ts
@@ -10,38 +10,36 @@ import { Location } from '@angular/common';
   templateUrl: './productos-datalle.component.html',
   styleUrl: './productos-datalle.component.css'
 })
-export class ProductosDatalleComponent implements OnInit{
+export class ProductosDatalleComponent implements OnInit {
 
-  productDetail? : Modelos
+  productDetail?: Modelos;
+  showFull: boolean = false;
 
   constructor(
-    private _modelosService : ModelosService,
-    private _router : ActivatedRoute,
-    private _cartService : CartService,
-    private location : Location
-  ){}
+    private _modelosService: ModelosService,
+    private _router: ActivatedRoute,
+    private _cartService: CartService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
-    this._router.params.subscribe( params => {
-      this._modelosService.getProductDetail( params['productoId']).subscribe( data => {
-          this.productDetail = data
+    this._router.params.subscribe(params => {
+      this._modelosService.getProductDetail(params['productoId']).subscribe(data => {
+        this.productDetail = data
       })
     })
   }
 
-  addCart(producto : Modelos, id : number){
-    this._cartService.addProductCart(producto); 
+  addCart(producto: Modelos, id: number) {
+    this._cartService.addProductCart(producto);
     this._cartService.addNumbersItem(id);
- }
+  }
 
- showFull: boolean = false;
+  toggleDescription() {
+    this.showFull = !this.showFull;
+  }
 
-toggleDescription() {
-  this.showFull = !this.showFull;
-}
-
-
-goBack(){
-  this.location.back()
-}
+  goBack() {
+    this.location.back()
+  }
 }
