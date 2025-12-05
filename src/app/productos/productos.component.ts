86abf1926870ab6d58bbeb7af3342aca32575aff
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-12-05 03:17:12 +0100

    bug listo

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css',
-  providers: [ModelosService]
+  
 })
 export class ProductosComponent implements OnInit{
     public productos: Modelos [];
